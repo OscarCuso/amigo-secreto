678f22f6dffaf855e81fd3d395f82369f5243b29
Oscar Cuevas <oscar.cuso20@gmail.com>
2025-07-28 12:03:00 -0600

    Se modifico la validacion de la funcion sortearAmigo

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 let amigos = [];
 
+// Agrega los amigos al arreglo
 function agregarAmigo(){
     let nombreAmigos = document.getElementById("amigo").value;
     
     if (nombreAmigos != ''){
-        // Verifica si se visualiza
         amigos.push(nombreAmigos)
         recorreAmigos()
         document.getElementById("amigo").value = '';
         
     }else{
-        alert('Por favor, Ingrese un nombre')
+        alert('Por favor, Inserte un nombre')
     }
 }
 
+// Se crea la lista de amigos y los muestra
 function recorreAmigos (){
     let listaAmigos = document.getElementById("listaAmigos")
     listaAmigos.innerHTML = ""
@@ -25,11 +26,14 @@ function recorreAmigos (){
     }
 }
 
+// Se realiza el sorteo y muetra el amigo sorteado
 function sortearAmigo (){
-    if (amigos != []){
+    if (amigos.length > 0){
         listaAmigos.innerHTML = ""
         let indice = parseInt(Math.floor(Math.random() * amigos.length))
         let resultado = document.getElementById("resultado")
         resultado.innerHTML = `El Amigo secreto sorteado es: ${amigos[indice]}`
+    }else{
+        alert ("Por favor antes de sortear ingresa los nombres")
     }
 }
