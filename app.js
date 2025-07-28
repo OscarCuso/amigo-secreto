b0cf0f7e69b2baf61fa86bc866760d846d15174b
Oscar Cuevas <oscar.cuso20@gmail.com>
2025-07-28 10:56:59 -0600

    Se agrega funcion para mostrar lista de amigos

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ let amigos = [];
 function agregarAmigo(){
     let nombreAmigos = document.getElementById("amigo").value;
     
-    if (!(nombreAmigos == '')){
+    if (nombreAmigos != ''){
         // Verifica si se visualiza
         amigos.push(nombreAmigos)
-        console.log(amigos)
+        recorreAmigos()
         document.getElementById("amigo").value = '';
+        
     }else{
         alert('Por favor, Ingrese un nombre')
+    }
+}
+
+function recorreAmigos (){
+    let listaAmigos = document.getElementById("listaAmigos")
+    listaAmigos.innerHTML = ""
+
+    for (let i = 0; i < amigos.length; i++) {
+        let lista = document.createElement("li")
+        lista.textContent = amigos[i]
+        listaAmigos.appendChild(lista)
     }
 }
